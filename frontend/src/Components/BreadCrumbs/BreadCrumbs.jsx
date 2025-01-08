dd8cdbd4fc76006c9d91fecddaa00be28609e0e0
Praneeta Verma <praneetav06@gmail.com>
2025-01-08 23:20:54 +0530

    added 2 new components

---
+++ frontend/src/Components/BreadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './BreadCrumbs.css';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
+const BreadCrumbs = (props) => { //mount the component in product page
+    const {product} = props;
+    return (
+        <div className="breadcrumb">
+            HOME <img src={arrow_icon} alt="arrow_icon" /> 
+            SHOP <img src={arrow_icon} alt="arrow_icon" />
+            {product.category} <img src={arrow_icon} alt="arrow_icon" />
+            {product.name}
+        </div>
+    )
+}
+export default BreadCrumbs;
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {shopContext} from '../Context/ShopContext';
+import {useParams} from 'react-router-dom';
+import BreadCrumbs from '../Components/BreadCrumbs/BreadCrumbs.jsx';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx';
 function Product() {
+    const {all_product} = useContext(shopContext); //string
+    const {productId} = useParams();
+    const product = all_product.find((e) => e.id === Number(productId)); //number
     return (
         <div>
-            
+            <BreadCrumbs product={product}/> 
+            <ProductDisplay product={product}/>
         </div>
     )
 }
