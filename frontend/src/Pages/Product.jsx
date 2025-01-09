5c5a59933ed4fdc557dc8823eb3487027be199df
Praneeta Verma <praneetav06@gmail.com>
2025-01-09 23:25:09 +0530

    made changes to products

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,18 +3,18 @@ import {shopContext} from '../Context/ShopContext';
 import {useParams} from 'react-router-dom';
 import BreadCrumbs from '../Components/BreadCrumbs/BreadCrumbs.jsx';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx';
-import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
-import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox.jsx';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts.jsx';
 function Product() {
     const {all_product} = useContext(shopContext); //string
     const {productId} = useParams();
     const product = all_product.find((e) => e.id === Number(productId)); //number
     return (
         <div>
-            <BreadCrumbs product={product}/> 
-            <ProductDisplay product={product}/>
-            <DescriptionBox/>
-            <RelatedProducts/>
+            <BreadCrumbs product={product}/>  
+            <ProductDisplay product={product}/> 
+            <DescriptionBox/> 
+            <RelatedProducts/> 
         </div>
     )
 }
