de32e800c8ab9a3e99da7f21ad8ca7b4fb22209d
Praneeta Verma <praneetav06@gmail.com>
2025-01-18 17:12:04 +0530

    added responsiveness

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Assets/nav_dropdown.png';
 function Navbar() {
     const [menu, setMenu] = useState("Shop");
+    const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
     return (
         <div className="navbar">
             <div className="nav-logo">
                 <img src={logo} alt="logo"/>
                 <p>SHOPPER</p>
             </div>
-            <ul className="nav-menu">
+            <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className="nav-menu">
                 <li onClick={() => {setMenu("Shop")}}> <Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu === "Shop" ? <hr /> : <></>}</li>
                 <li onClick={() => {setMenu("Men")}}> <Link style={{textDecoration: 'none'}} to='/Men'>Men</Link>{menu === "Men" ? <hr /> : <></>}</li>
                 <li onClick={() => {setMenu("Women")}}> <Link style={{textDecoration: 'none'}} to='/Women'>Women</Link>{menu === "Women" ? <hr /> : <></>}</li>
@@ -20,7 +29,7 @@ function Navbar() {
             <div className="nav-login-cart">
                 <Link to="/Login"><button>Login</button></Link>
                 <Link to="/Cart"><img src={cart_icon} alt="cart_icon"/></Link>
-                <div className="nav-cart-count">0</div>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
         </div>
     );
