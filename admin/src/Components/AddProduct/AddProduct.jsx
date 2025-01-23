2217b21c80ecd950815f75541e5f1340879a129b
Praneeta Verma <praneetav06@gmail.com>
2025-01-23 23:21:29 +0530

    added admin sidebar panel items

---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AddProduct.css';
+
+const AddProduct = () => {
+    return (
+        <div className='add-product'>
+            <div className="addproduct-itemfield">
+                <p>Product title</p>
+                <input type="text" name='name' placeholder='Type Here...' />
+            </div>
+
+        </div>
+    )
+}
+
+export default AddProduct;
