0005c29ce30a36af47121bf7b97660cc5795374b
Praneeta Verma <praneetav06@gmail.com>
2025-03-13 17:36:21 +0530

    reset code

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Hero from '../Components/Hero/Hero.jsx';
-import Popular from '../Components/Popular/Popular.jsx';
-import Offers from '../Components/Offers/Offers.jsx';
-import NewCollections from '../Components/NewCollections/NewCollections.jsx';
-import NewsLetter from '../Components/NewsLetter/NewsLetter.jsx';
+import React from "react";
+import Hero from "../Components/Hero/Hero.jsx";
+import Popular from "../Components/Popular/Popular.jsx";
+import Offers from "../Components/Offers/Offers.jsx";
+import NewCollections from "../Components/NewCollections/NewCollections.jsx";
+import NewsLetter from "../Components/NewsLetter/NewsLetter.jsx";
 function Shop() {
-    return (
-        <div>
-            <Hero />
-            <Popular />
-            <Offers/>
-            <NewCollections/>
-            <NewsLetter/>
-        </div>
-    )
+  return (
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
+    </div>
+  );
 }
 export default Shop;
