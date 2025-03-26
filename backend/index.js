427ded768c74eff97ee58edb396fbd2999a9bfb9
Praneeta Verma <praneetav06@gmail.com>
2025-03-26 15:36:22 +0530

    edited port value

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
-const port = 4000;
+const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
